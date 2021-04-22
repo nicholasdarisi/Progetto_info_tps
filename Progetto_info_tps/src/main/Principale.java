5b3a5ee774157c7bcadc62662e58926e5216809d
DESKTOP-TIDRHG5\ruize <nalanchangge@gmail.com>
2021-04-22 10:35:01 +0200

    Test

---
+++ Progetto_info_tps/src/main/Principale.java
@@ -0,0 +1,22 @@
+package Main;
+
+import java.awt.EventQueue;
+
+
+import View.Grafica;
+
+public class Principale {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Grafica window = new Grafica();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
